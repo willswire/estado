6e40cc74dc9826d627aeab179617bbb6119fe4a9
William Walker <w_walker@icloud.com>
2024-07-05 14:01:13 -0500

    squash: remove unusued endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import { OpenAPIRouter } from "@cloudflare/itty-router-openapi";
 import { TaskCreate } from "./endpoints/taskCreate";
 import { TaskDelete } from "./endpoints/taskDelete";
 import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 
 export const router = OpenAPIRouter({
 	docs_url: "/",
 });
 
-router.get("/states/:projectName", TaskList);
 router.post("/api/tasks/", TaskCreate);
 router.get("/api/tasks/:taskSlug/", TaskFetch);
 router.delete("/api/tasks/:taskSlug/", TaskDelete);
