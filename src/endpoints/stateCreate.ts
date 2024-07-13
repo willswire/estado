870d9614132e31e1391ccb5fbf2e078760c1378c
William Walker <w_walker@icloud.com>
2024-07-12 19:46:15 -0500

    feat: remove auth

--- src/endpoints/stateCreate.ts
+++ src/endpoints/stateCreate.ts
@@ -1,75 +1,67 @@
-// stateCreate.ts
 import {
-    OpenAPIRoute,
-    OpenAPIRouteSchema,
-    Path,
-	Query
+  OpenAPIRoute,
+  OpenAPIRouteSchema,
+  Path,
+  Query,
 } from "@cloudflare/itty-router-openapi";
-import { checkAuthorization } from '../utils';
 
 export class StateCreate extends OpenAPIRoute {
-    static schema: OpenAPIRouteSchema = {
-        tags: ["States"],
-        summary: "Create or update the remote state.",
-        parameters: {
-            projectName: Path(String, {
-                description: "Project name",
-            }),
-			ID: Query(String, {
-				description: "The state lock identifier",
-				required: false
-			})
+  static schema: OpenAPIRouteSchema = {
+    tags: ["States"],
+    summary: "Create or update the remote state.",
+    parameters: {
+      projectName: Path(String, {
+        description: "Project name",
+      }),
+      ID: Query(String, {
+        description: "The state lock identifier",
+        required: false,
+      }),
+    },
+    responses: {
+      "200": {
+        description: "State created or updated successfully",
+        schema: {
+          type: "string",
         },
-        responses: {
-            "200": {
-                description: "State created or updated successfully",
-                schema: {
-                    type: "string"
-                }
-            },
-            "400": {
-                description: "No project name specified",
-                schema: {
-                    type: "string"
-                }
-            },
-            "401": {
-                description: "Invalid authentication credentials",
-                schema: {
-                    type: "string"
-                }
-            }
-        }
-    };
+      },
+      "400": {
+        description: "No project name specified",
+        schema: {
+          type: "string",
+        },
+      },
+      "401": {
+        description: "Invalid authentication credentials",
+        schema: {
+          type: "string",
+        },
+      },
+    },
+  };
 
-    async handle(
-        request: Request,
-        env: any,
-        context: any,
-        data: Record<string, any>
-    ) {
-        const auth = checkAuthorization(request);
-        if (!auth) {
-            return new Response("Invalid authentication credentials", {status: 401});
-        }
+  async handle(
+    request: Request,
+    env: any,
+    context: any,
+    data: Record<string, any>,
+  ) {
+    const { projectName } = data.params;
 
-        const { username } = auth;
-        const { projectName } = data.params;
-
-        if (!projectName) {
-            return new Response("No project name specified", {status: 400});
-        }
-
-        const requestData = await request.arrayBuffer();
-        if (!requestData) {
-            return new Response("Invalid request data", {status: 400});
-        }
-
-		const key = `${username}/${projectName}.tfstate`
-		await env.TF_STATE_BUCKET.put(key, requestData);
-
-        console.log("Created or updated state data for", projectName);
-
-        return new Response("State created or updated successfully");
+    if (!projectName) {
+      return new Response("No project name specified", { status: 400 });
     }
+
+    const requestData = await request.arrayBuffer();
+    if (!requestData) {
+      return new Response("Invalid request data", { status: 400 });
+    }
+
+    const key = `${projectName}.tfstate`;
+    await env.TF_STATE_BUCKET.put(key, requestData);
+
+    console.log("Created or updated state data for", projectName);
+
+    return new Response("State created or updated successfully");
+  }
 }
--- src/endpoints/stateLock.ts
+++ src/endpoints/stateLock.ts
@@ -1,107 +1,90 @@
-// stateLock.ts
 import {
-    OpenAPIRoute,
-    OpenAPIRouteSchema,
-    Path
+  OpenAPIRoute,
+  OpenAPIRouteSchema,
+  Path,
 } from "@cloudflare/itty-router-openapi";
 import type { LockInfo } from "../types/terraform";
-import { checkAuthorization } from '../utils';
 
 export class StateLock extends OpenAPIRoute {
-    static schema: OpenAPIRouteSchema = {
-        tags: ["States"],
-        summary: "Lock or Unlock the remote state for edits.",
-        parameters: {
-            projectName: Path(String, {
-                description: "Project name",
-            })
+  static schema: OpenAPIRouteSchema = {
+    tags: ["States"],
+    summary: "Lock or Unlock the remote state for edits.",
+    parameters: {
+      projectName: Path(String, {
+        description: "Project name",
+      }),
+    },
+    responses: {
+      "200": {
+        description: "Returns if the state was locked successfully",
+        schema: {
+          type: "string",
         },
-        responses: {
-            "200": {
-                description: "Returns if the state was locked successfully",
-                schema: {
-                    type: "string"
-                }
-            },
-            "400": {
-                description: "No project name specified",
-                schema: {
-                    type: "string"
-                }
-            },
-            "401": {
-                description: "Invalid authentication credentials",
-                schema: {
-                    type: "string"
-                }
-            },
-            "405": {
-                description: "Method not allowed",
-                schema: {
-                    type: "string"
-                }
-            },
-            "423": {
-                description: "State is currently locked",
-                schema: {
-                    type: "string"
-                }
-            },
-            "500": {
-                description: "Unable to determine username",
-                schema: {
-                    type: "string"
-                }
-            }
-        }
-    };
+      },
+      "400": {
+        description: "No project name specified",
+        schema: {
+          type: "string",
+        },
+      },
+      "405": {
+        description: "Method not allowed",
+        schema: {
+          type: "string",
+        },
+      },
+      "423": {
+        description: "State is currently locked",
+        schema: {
+          type: "string",
+        },
+      },
+      "500": {
+        description: "Unable to determine username",
+        schema: {
+          type: "string",
+        },
+      },
+    },
+  };
 
-    async handle(
-        request: Request,
-        env: any,
-        context: any,
-        data: Record<string, any>
-    ) {
-        const auth = checkAuthorization(request);
-        if (!auth) {
-            return new Response("Invalid authentication credentials", {status: 401})
-        }
+  async handle(
+    request: Request,
+    env: any,
+    context: any,
+    data: Record<string, any>,
+  ) {
+    const { projectName } = data.params;
 
-        const { username } = auth;
-        const { projectName } = data.params;
-
-        if (!projectName) {
-            return new Response("No project name specified", {status: 400})
-        }
-
-        const key = `${username}/${projectName}.tfstate`;
-        const id = env.TF_STATE_LOCK.idFromName(key);
-        const stub = env.TF_STATE_LOCK.get(id);
-        const lockInfo: LockInfo = await stub.getLockInfo();
-        const newLock = await request.json() as LockInfo || null;
-
-        switch (request.method) {
-            case "GET":
-                return new Response(
-                    JSON.stringify(lockInfo),
-                    { status: 200 }
-                );
-            case "PUT":
-            case "LOCK":
-                if (await stub.lock(newLock)) {
-                    return new Response("Acquired state lock")
-                } else {
-                    return new Response("State is currently locked", {status: 423})
-                }
-            case "DELETE":
-            case "UNLOCK":
-                if (await stub.unlock(newLock)) {
-                    return new Response("Unlocked state")
-                } else {
-                    return new Response("State is currently locked", {status: 423})
-                }
-            default:
-                return new Response("Method not allowed", {status: 405})
-        }
+    if (!projectName) {
+      return new Response("No project name specified", { status: 400 });
     }
+
+    const key = `${projectName}.tfstate`;
+    const id = env.TF_STATE_LOCK.idFromName(key);
+    const stub = env.TF_STATE_LOCK.get(id);
+    const lockInfo: LockInfo = await stub.getLockInfo();
+    const newLock = ((await request.json()) as LockInfo) || null;
+
+    switch (request.method) {
+      case "GET":
+        return new Response(JSON.stringify(lockInfo), { status: 200 });
+      case "PUT":
+      case "LOCK":
+        if (await stub.lock(newLock)) {
+          return new Response("Acquired state lock");
+        } else {
+          return new Response("State is currently locked", { status: 423 });
+        }
+      case "DELETE":
+      case "UNLOCK":
+        if (await stub.unlock(newLock)) {
+          return new Response("Unlocked state");
+        } else {
+          return new Response("State is currently locked", { status: 423 });
+        }
+      default:
+        return new Response("Method not allowed", { status: 405 });
+    }
+  }
 }
